7fe5438f02fda0e093de84842a0e5e12b7075060
Valery Kharitonov <val@kharvd.com>
2023-03-02 21:26:55 -0500

    update batch size to 2048

--- week02_management_and_testing/homework/tests/test_pipeline.py
+++ week02_management_and_testing/homework/tests/test_pipeline.py
@@ -62,7 +62,7 @@ def test_training(device, tmp_path, train_dataset):
     ddpm.to(device)
 
     optim = torch.optim.Adam(ddpm.parameters(), lr=5e-4)
-    dataloader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+    dataloader = DataLoader(train_dataset, batch_size=2048, shuffle=True)
 
     train_epoch(ddpm, dataloader, optim, device)
     generate_samples(ddpm, device, tmp_path)
